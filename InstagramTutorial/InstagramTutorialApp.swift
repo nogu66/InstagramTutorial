523495f3918276c4032f3658f6cd3bfae178275b
nogu66 <ezochi66@gmail.com>
2023-05-31 23:11:12 +0900

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by YutaNoguchi on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
