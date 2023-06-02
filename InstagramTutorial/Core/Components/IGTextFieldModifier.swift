8186014b9d89909c58cb34a86b721a2c42864094
nogu66 <ezochi66@gmail.com>
2023-06-02 22:07:20 +0900

    ログインとサインインビュー

---
+++ InstagramTutorial/Core/Components/IGTextFieldModifier.swift
@@ -0,0 +1,19 @@
+//
+//  IGTextFieldModifier.swift
+//  InstagramTutorial
+//
+//  Created by YutaNoguchi on 2023/06/02.
+//
+
+import SwiftUI
+
+struct IGTextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .padding(12)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            .padding(.horizontal,24)
+    }
+}
